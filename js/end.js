67d2be0f2d433153d6c5401f9a990fdec45dc1a3
Ninad Sapate <nsapate12@gmail.com>
2026-03-20 02:12:15 +0000

    Restore working game shell and refresh opening level

--- js/end.js
+++ js/end.js
@@ -1,48 +1,28 @@
 Game.Endd = function (game) { };
 
 Game.Endd.prototype = {
-    create: function () {
-        game.camera.follow(null);
-        game.camera.setPosition(0, 0);
+	create: function () {
+		game.camera.follow(null);
+		game.camera.setPosition(0,0);
 
-        GameShell.setStatus('Run Complete', 'All levels cleared. Start another run whenever you want.');
-        GameShell.setStartLabel('Restart Run');
+		this.cursor = this.game.input.keyboard.createCursorKeys();
+		var logo = game.add.sprite(w/2, 300, 'success');
+		logo.anchor.setTo(0.5, 0.5);
+		logo.scale.setTo(0, 0);
+		game.add.tween(logo.scale).to({ x:1, y:1 }, 1000, Phaser.Easing.Bounce.Out).start();
 
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.enterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-        this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		var label = game.add.text(w/2, h-100, 'you died '+dead+' times\n\npress the ↑ key to restart', { font: '25px Arial', fill: '#ceb882', align: 'center' });
+		label.anchor.setTo(0.5, 0.5);
+		label.alpha = 0;
+		game.add.tween(label).delay(500).to({ alpha: 1}, 500).start();
 
-        var logo = game.add.sprite(w / 2, 300, 'success');
-        logo.anchor.setTo(0.5, 0.5);
-        logo.scale.setTo(0, 0);
-        game.add.tween(logo.scale).to({ x: 1, y: 1 }, 1000, Phaser.Easing.Bounce.Out, true);
+		game.add.tween(label).to({ angle:1 }, 500).to({ angle:-1 }, 500).loop().start();
 
-        var label = game.add.text(
-            w / 2,
-            h - 100,
-            'You died ' + dead + ' times\n\nPress Up, Enter, Space, or Start to replay',
-            { font: '25px Arial', fill: '#ceb882', align: 'center' }
-        );
-        label.anchor.setTo(0.5, 0.5);
-        label.alpha = 0;
-        game.add.tween(label).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 500);
-        game.add.tween(label).to({ angle: 1 }, 500).to({ angle: -1 }, 500).loop().start();
+		this.time = this.game.time.now + 500;
+	},
 
-        this.time = this.game.time.now + 500;
-    },
-
-    update: function () {
-        if ((this.cursor.up.justDown || this.enterKey.justDown || this.spaceKey.justDown) && this.time < this.game.time.now) {
-            this.beginPlay();
-        }
-    },
-
-    beginPlay: function () {
-        if (this.time >= this.game.time.now) {
-            return;
-        }
-
-        dead = 0;
-        game.state.start('Play');
-    }
+	update: function() {
+		if (this.cursor.up.isDown && this.time < this.game.time.now)
+			game.state.start('Play');
+	}
 };
--- js/menu.js
+++ js/menu.js
@@ -1,103 +1,76 @@
 Game.Menu = function (game) { };
 
+var timeline;
+
 Game.Menu.prototype = {
-    create: function () {
-        GameShell.setStatus('Ready', 'Press Up, Enter, Space, or the Start button to begin the run.');
-        GameShell.setStartLabel('Start Run');
 
-        this.cursor = this.game.input.keyboard.createCursorKeys();
-        this.enterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
-        this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	preload: function() {
 
-        this.introMusic = game.add.audio('intro');
-        this.logo = game.add.sprite(w / 2, h + 120, 'logo');
-        this.logo.anchor.setTo(0.5, 0.5);
-        this.logoTween = game.add.tween(this.logo).to({ y: h / 2 - 15 }, 1400, Phaser.Easing.Cubic.Out, true);
+	},
 
-        this.label = game.add.text(
-            w / 2,
-            h - 70,
-            'PRESS UP, ENTER, OR SPACE TO START',
-            { font: '28px Arial', fill: '#d9edf0', align: 'center' }
-        );
-        this.label.anchor.setTo(0.5, 0.5);
-        this.label.alpha = 0;
-        this.labelTween = game.add.tween(this.label).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 400, -1, true);
+	create: function () {
+		this.cursor = this.game.input.keyboard.createCursorKeys();
+		this.intromusic = game.add.audio('intro');
+		this.intromusic.play("", 0, 1, true);
+		var logo = game.add.sprite(w/2, h+150, 'logo');
+		logo.anchor.setTo(0.5, 0.5);
+		game.add.tween(logo).to({ y: h/2 }, 2000, Phaser.Easing.Bounce.Out).start();
 
-        this.soundToggle = this.game.add.button(w - 70, 50, 'sound', this.toggleSound, this);
-        this.soundToggle.input.useHandCursor = true;
-        this.soundToggle.alpha = 0.92;
+		var label = game.add.text(w/2, h-50, 'press the ↑ arrow key to start', { font: '30px Arial', fill: '#8f9d9d' });
+		label.anchor.setTo(0.5, 0.5);
+		label.alpha = 0;
+		game.add.tween(label).delay(500).to({ alpha: 1}, 500).start();
 
-        this.player = game.add.sprite(this.logo.x + 69 - this.logo.width / 2, -80, 'player');
-        this.player.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15, true);
-        this.player.animations.frame = 9;
+		this.sound_toggle = this.game.add.button(w-70, 50, 'sound', this.toggle_sound, this);
+		this.sound_toggle.alpha = 0;
+		this.sound_toggle.inputEnabled = true;
+		this.sound_toggle.input.useHandCursor = true;
+		game.add.tween(this.sound_toggle).delay(500).to({ alpha: 1}, 500).start();
 
-        this.playerDropTween = game.add.tween(this.player).to(
-            { y: h / 2 - this.logo.height / 2 - 30 },
-            1200,
-            Phaser.Easing.Cubic.Out,
-            true
-        );
-        this.playerDropTween.onComplete.add(function () {
-            this.player.animations.play('walk');
-            this.playerPatrolTween = game.add.tween(this.player).to(
-                { x: this.logo.x + this.logo.width / 2 - 137 },
-                2600,
-                Phaser.Easing.Linear.None,
-                true,
-                0,
-                -1,
-                true
-            );
-        }, this);
+		game.add.tween(label).to({ angle:5 }, 500).to({ angle:-1 }, 500).loop().start();
 
-        this.refreshSound();
-    },
+		var player = game.add.sprite(logo.x+69-logo.width/2, -100, 'player');
 
-    update: function () {
-        if (this.cursor.up.justDown || this.enterKey.justDown || this.spaceKey.justDown) {
-            this.beginPlay();
-        }
-    },
+    	var walk = player.animations.add('walk', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15, true);
 
-    beginPlay: function () {
-        if (this.introMusic && this.introMusic.isPlaying) {
-            this.introMusic.stop();
-        }
+    	timeline = new TimelineMax();
+    	player.animations.frame = 9;
+    	timeline.insert(TweenMax.to(player, 2, {y:h/2-logo.height/2-30, ease:Linear.easeNone, onComplete: function() {
+    		walk.play();
+    	}}));
+    	timeline.append(TweenMax.to(player, 3, {x:logo.x+logo.width/2-137, repeat:Number.MAX_VALUE, yoyo:true,ease:Linear.easeNone}), 0.5);
+    	this.game.onBlur.add(TweenMax.pauseAll, TweenMax);
+    	this.game.onFocus.add(TweenMax.resumeAll, TweenMax);
 
-        game.state.start('Play');
-    },
+	},
 
-    refreshSound: function () {
-        if (!this.soundToggle) {
-            return;
-        }
+	update: function() {
+		if (this.cursor.up.isDown) {
+			this.intromusic.stop();
+			TweenMax.pauseAll();
+			this.game.state.start('Play');
+		}
+		if (!sound) {
+			this.intromusic.pause();
+		}
+		else {
+			this.intromusic.resume();
+		}
 
-        this.soundToggle.frame = sound ? 0 : 1;
+	},
 
-        if (!this.introMusic) {
-            return;
-        }
+	toggle_sound: function() {
+		if (this.sound_toggle.frame == 0) {
+			this.sound_toggle.frame = 1;
+			sound = false;
+		}
+		else {
+			this.sound_toggle.frame = 0;
+			sound = true;
+		}
+	},
 
-        if (!sound) {
-            this.introMusic.stop();
-            return;
-        }
+	render: function () {
 
-        if (!this.introMusic.isPlaying) {
-            this.introMusic.play('', 0, 0.6, true);
-        }
-    },
-
-    toggleSound: function () {
-        sound = !sound;
-        GameShell.syncSound();
-        this.refreshSound();
-    },
-
-    shutdown: function () {
-        if (this.introMusic) {
-            this.introMusic.stop();
-        }
-    }
+	}
 };
